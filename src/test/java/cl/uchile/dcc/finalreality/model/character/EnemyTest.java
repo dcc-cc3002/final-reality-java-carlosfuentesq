55401bdc87c581d046417e93e2e96471c3b5f360
Carlos Fuentes Quezada <fuentesquezadacarlos@gmail.com>
2022-11-09 23:55:31 -0300

    Se agregan tests en EnemyTest

--- src/test/java/cl/uchile/dcc/finalreality/model/character/EnemyTest.java
+++ src/test/java/cl/uchile/dcc/finalreality/model/character/EnemyTest.java
@@ -13,18 +13,30 @@ import static org.junit.Assert.*;
 public class EnemyTest {
   private GameCharacter enemy1;
   private GameCharacter enemy2;
+  private GameCharacter enemy3;
   private GameCharacter knight1;
 
   @Before
   public void setUp() throws InvalidStatValueException {
     BlockingQueue<GameCharacter> queue = new LinkedBlockingQueue<>();
     enemy1 = new Enemy("Enemy 1",10,20,15,queue);
-    enemy2 = new Enemy("Enemy 2",10,20,15,queue);
+    enemy2 = new Enemy("Enemy 2",15,25,10,queue);
+    enemy3 = new Enemy("Enemy 1",10,20,15,queue);
     knight1 = new Knight("Knight 1", 30, 5, queue);
   }
 
   @Test
-  public void getWeight() {
+  public void testGettersAndSetters() throws InvalidStatValueException {
+    String expectedName = "Enemy 2";
+    assertEquals(expectedName, enemy2.getName());
+    int expectedHp = 25;
+    assertEquals(expectedHp, enemy2.getCurrentHp());
+    assertEquals(enemy2.getCurrentHp(), enemy2.getMaxHp());
+    enemy2.setCurrentHp(15);
+    expectedHp = 15;
+    assertEquals(expectedHp, enemy2.getCurrentHp());
+    int expectedDefense = 10;
+    assertEquals(expectedDefense, enemy2.getDefense());
   }
 
   @Test
@@ -33,5 +45,9 @@ public class EnemyTest {
 
   @Test
   public void testEquals() {
+    assertEquals(enemy1, enemy1);
+    assertNotEquals(enemy1, enemy2);
+    assertEquals(enemy1, enemy3);
+    assertNotEquals(enemy1, knight1);
   }
 }
