fde200f6fca21a67fb920e45f1fa29b861965da5
Carlos Fuentes Quezada <fuentesquezadacarlos@gmail.com>
2022-11-30 20:51:15 -0300

    Se agregan métodos para crear personajes

--- src/main/java/cl/uchile/dcc/finalreality/GameController.java
+++ src/main/java/cl/uchile/dcc/finalreality/GameController.java
@@ -1,8 +1,10 @@
 package cl.uchile.dcc.finalreality;
 
+import cl.uchile.dcc.finalreality.exceptions.InvalidStatValueException;
 import cl.uchile.dcc.finalreality.model.character.Enemy;
 import cl.uchile.dcc.finalreality.model.character.GameCharacter;
-import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter;
+import cl.uchile.dcc.finalreality.model.character.player.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,4 +29,37 @@ public class GameController {
     for (Enemy enemy : enemyCharacters)
       enemy.waitTurn();
   }
+  public void createBlackMage(String name, int maxHp, int defense, int maxMp)
+      throws InvalidStatValueException {
+    playerCharacters.add(new BlackMage(name, maxHp, defense, maxMp, turnsQueue));
+  }
+
+  public void createEngineer(String name, int maxHp, int defense)
+      throws InvalidStatValueException {
+    playerCharacters.add(new Engineer(name, maxHp, defense, turnsQueue));
+  }
+
+  public void createKnight(String name, int maxHp, int defense)
+      throws InvalidStatValueException {
+    playerCharacters.add(new Knight(name, maxHp, defense, turnsQueue));
+  }
+
+  public void createThief(String name, int maxHp, int defense)
+      throws InvalidStatValueException {
+    playerCharacters.add(new Thief(name, maxHp, defense, turnsQueue));
+  }
+
+  public void createWhiteMage(String name, int maxHp, int defense, int maxMp)
+      throws InvalidStatValueException {
+    playerCharacters.add(new WhiteMage(name, maxHp, defense, maxMp, turnsQueue));
+  }
+
+  public void createEnemy(String name, int weight, int maxHp, int defense)
+      throws InvalidStatValueException {
+    enemyCharacters.add(new Enemy(name, weight, maxHp, defense, turnsQueue));
+  }
+
+  
+
+
 }
